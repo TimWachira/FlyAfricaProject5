4a94dcaaf82c23511243642b8cc0d6cd79587aa5
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-06-12 11:55:08 +0300

    incoportared the js collapsibles

--- FAQ.php
+++ FAQ.php
@@ -12,7 +12,7 @@
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About Us</a></li>
+            <li><a href="About Us.php">About Us</a></li>
             <li><a href="destinations.php">Destinations</a></li>
             <li><a href="SkyLoyalty.php">SkyLoyalty</a></li>
             <li><a href="Experiences.php">Experiences</a></li>
