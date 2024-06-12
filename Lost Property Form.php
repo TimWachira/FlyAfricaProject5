3398a75564e9ec898ff57b265c977ea5619ecfc4
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-06-12 16:33:20 +0300

    Created the Lost Property Form page

---
+++ Lost Property Form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lost Property - Fly Africa</title>
+    <link rel="stylesheet" href="FAQ Styles.css">
+    <link rel="icon" href="images/icons8-airplane-48.png">
+</head>
+<body>
+    <div class="container">
+        <h2>Lost Property Form</h2>
+        <form action="/submit-lost-property" method="post">
+           
