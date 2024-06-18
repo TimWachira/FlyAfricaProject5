a8a7094e9350f597a61db7f94744233ce2d0a72d
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-06-18 23:01:01 +0300

    Added php code and connected to thedb to display message when sent button is clicked

--- Contact Us.php
+++ Contact Us.php
@@ -6,7 +6,6 @@
     <title>FlyAfrica - Contact Us</title>
     <link rel="stylesheet" href="ContactUs Styles.css">
     <link rel="icon" href="images/icons8-airplane-48.png">
-    
 </head>
 <body>
     
@@ -26,11 +25,10 @@
     <!-- Contact Page Details -->
     <div class="container">
         <h1>Contact FlyAfrica</h1>
-        <p>Have a question or feedback? Get in touch with us using the forms below. 
-        </p>
+        <p>Have a question or feedback? Get in touch with us using the form below.</p>
 
-        <!--Contact Form  -->
-        <form action="#" method="post">
+        <!-- Contact Form -->
+        <form action="Contact Us.php" method="post">
             <label for="name">Your Name: </label>
             <input type="text" id="name" name="name" required>
 
@@ -40,8 +38,45 @@
             <label for="message">Your Message: </label>
             <textarea id="message" name="message" required></textarea>
 
-            <input type="submit" value="Send Message">
+            <input type="submit" name="submit" value="Send Message">
         </form>
+        
+        <?php
+
+        // Checks if form is submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "flights";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Prepares to Fetch User Info and Insert into Database//
+            $stmt = $conn->prepare("INSERT INTO Contacts (name, email, message) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $name, $email, $message);
+
+            // Set parameters and execute
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+            $stmt->execute();
+
+            // Close statement and connection
+            $stmt->close();
+            $conn->close();
+
+            // Provide feedback to the user
+            echo "<p>Thankyou! Your Message has been sent successfully!</p>";
+        }
+        ?>
     </div>
 </body>
 </html>
