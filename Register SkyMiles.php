8ae40fec1626e5cc413826c9ee2196cf9da3c22f
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-06-18 21:07:18 +0300

    Connected the DB to Register Page - User can input values and will go directly to the DB

--- Register SkyMiles.php
+++ Register SkyMiles.php
@@ -23,23 +23,59 @@
 
     <div class="container">
         <h2>Register</h2>
-        <form action="/register" method="post">
+        <form action="Register.php" method="post">
             <div class="form-group">
-                <label for="new-username">Username</label>
-                <input type="text" id="new-username" name="new-username" required>
+                <label for="user_name">Username</label>
+                <input type="text" id="user_name" name="user_name" required>
             </div>
             <div class="form-group">
-                <label for="new-password">Password</label>
-                <input type="password" id="new-password" name="new-password" required>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" required>
             </div>
             <div class="form-group">
-                <button type="submit">Register</button>
+            <button type = 'submit'> <a href="Register.php" class="button">Register</a>
             </div>
         </form>
     </div>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Database connection details
+            $servername = "localhost"; 
+            $username = "root"; 
+            $password = "";  
+            $database = "flights"; 
+    
+            $conn = mysqli_connect($servername, $username, $password, $database);
+            
+            // Check connection
+            if($conn === false){
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+            
+            // Taking all 3 values from the form data(input)
+            $user_name = $_POST['user_name'];
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+            
+            // Performing insert query execution
+            // Here, specify the columns explicitly
+            $sql = "INSERT INTO users (username, password, email) VALUES ('$user_name', '$password', '$email')";
+            
+            if(mysqli_query($conn, $sql)){
+                echo "<h3>Data stored in the database successfully.</h3>"; 
+                echo nl2br("Username: $user_name\nPassword: $password\nEmail: $email");
+            } else{
+                echo "ERROR: Hush! Sorry $sql. " . mysqli_error($conn);
+            }
+            
+            // Close connection
+            mysqli_close($conn);
+        }
+        ?>
+    
 </body>
 </html>
