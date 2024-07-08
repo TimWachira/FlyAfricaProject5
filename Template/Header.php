ac92907efe75a5c15ef08e04cd64ed7f148c4906
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-07-08 10:57:42 +0300

    Added the Include folder for code effciency

---
+++ Template/Header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlyAfrica - About Us</title>
+    <link rel="icon" href="images/icons8-airplane-48.png">
+</head>
---
+++ Template/Nav.php
@@ -0,0 +1,12 @@
+ <!-- Navigation List -->
+ <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="About Us.php">About Us</a></li>
+            <li><a href="destinations.php">Destinations</a></li>
+            <li><a href="SkyLoyalty.php">SkyLoyalty</a></li>
+            <li><a href="experiences.php">Experiences</a></li>
+            <li><a href="Contact Us.php">Contact Us</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+        </ul>
+    </nav>
