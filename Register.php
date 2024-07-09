81167b9f2a1d61c0064458219663ff50d0e50092
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-07-09 08:11:48 +0300

    Added the include and requre templates

--- Register.php
+++ Register.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link rel="stylesheet" href="Register Styles.css">
-    <link rel="icon" href="images/icons8-airplane-48.png">
-</head>
-<body>
-<!-- Navigation List -->
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="About Us.php">About Us</a></li>
-        <li><a href="destinations.php">Destinations</a></li>
-        <li><a href="SkyLoyalty.php">SkyLoyalty</a></li>
-        <li><a href="Experiences.php">Experiences</a></li>
-        <li><a href="Contact Us.php">Contact Us</a></li>
-        <li><a href="faq.php">FAQ</a></li>
-        <li><a href="View_Users.php">FlyAfrica Users</a></li>
-    </ul>
-</nav>
+<?php 
+    include_once("Template/Header.php");
+    include_once("Template/Nav.php");
+    require("Template/DbConn.php");
+    ?>
 
 <div class="container">
     <h2>Register</h2>
