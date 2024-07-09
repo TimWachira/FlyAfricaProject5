310fff2e66a50596ee559a5ae3ce55f59f9f5ff5
Tim Wachira <timothy.kinuu@strathmore.edu>
2024-07-09 08:47:49 +0300

    Adjusted the Nav Template

--- Template/Nav.php
+++ Template/Nav.php
@@ -8,5 +8,6 @@
             <li><a href="experiences.php">Experiences</a></li>
             <li><a href="Contact Us.php">Contact Us</a></li>
             <li><a href="faq.php">FAQ</a></li>
+            <li><a href="View_Users.php">FlyAfrica Users</a></li>
         </ul>
     </nav>
